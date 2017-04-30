541954f9667d0c8f57c528c596532cadbac6b902
Roman Kunin <mosceo@gmail.com>
2017-04-30 15:54:44 +1000

    finish part2/summary

--- part2/1.rkt
+++ part2/1.rkt
@@ -1,32 +1,57 @@
 ;; The first three lines of this file were inserted by DrRacket. They record metadata
 ;; about the language level of this file in a form that our tools can easily process.
 #reader(lib "htdp-beginner-abbr-reader.ss" "lang")((modname |1|) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
-;(require 2htdp/batch-io)
-;------------------------
+; List-of-numbers -> ... nested list ...
+; creates a row for an HTML table from l
+(check-expect (make-row '(1 "title")) '(tr (td "1") (td "title")))
 
-; List -> List
-; produce a list of prefixes
-(define (prefixes l)
-  (cond [(empty? l) (list '())]
-        [else (cons '()
-                    (prepend (first l) (prefixes (rest l))))]))
-
-; Any List[List]
-; add item 'i' to every element (also a list) of the list as the first element
-(check-expect (prepend 1 (list empty)) (list (list 1)))
-(check-expect (prepend 1 empty) empty)
-(check-expect (prepend 1 (list (list "a"))) (list (list 1 "a")))
-(check-expect (prepend 1 (list (list "a") (list "a" "b") (list "a" "b" "c")))
-              (list (list 1 "a") (list 1 "a" "b") (list 1 "a" "b" "c")))
-
-(define (prepend i l)
-  (cond [(empty? l) '()]
-        [else (cons (cons i (first l))
-                    (prepend i (rest l)))]))
+(define (make-row l)
+  `(tr (td ,(number->string (first l)))
+       (td ,(second l))))
 
 
+;-------------
+(check-expect (ranking '("flag" "mask" "fire"))
+              '((1 "flag") (2 "mask") (3 "fire")))
+(define (ranking los)
+  (reverse (add-ranks (reverse los))))
 
 
+;-------------
+(check-expect (add-ranks '("flag" "mask" "fire"))
+              '((3 "flag") (2 "mask") (1 "fire")))
+(define (add-ranks los)
+  (cond
+    [(empty? los) '()]
+    [else (cons (list (length los) (first los))
+                (add-ranks (rest los)))]))
+
+
+
+
+
+
+;------------
+(define (make-ranking los)
+  (make-ranking-aux (ranking los)))
+ы
+
+;-------------
+(check-expect (make-ranking-aux '((1 "title1") (2 "title2")))
+                                `(,(make-row '(1 "title1"))
+                                  ,(make-row '(2 "title2"))))
+(define (make-ranking-aux lst)
+  (cond [(empty? lst) '()]
+        [else (cons (make-row (first lst))
+                    (make-ranking-aux (rest lst)))]))
+
+               
+(define one-list
+  '("Asia: Heat of the Moment"
+    "U2: One"
+    "The White Stripes: Seven Nation Army"))
+
+(make-ranking one-list)
 
 
 
