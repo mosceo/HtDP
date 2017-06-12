3cb1e79f340d0cb8e17f24edb49c7c4725502a7e
Roman Kunin <mosceo@gmail.com>
2017-06-12 13:27:10 +1000

    work on part5

--- part5/1.rkt
+++ part5/1.rkt
@@ -2,28 +2,10 @@
 ;; about the language level of this file in a form that our tools can easily process.
 #reader(lib "htdp-intermediate-lambda-reader.ss" "lang")((modname |1|) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
 (require 2htdp/abstraction)
+(require 2htdp/image)
 ;--------------------------
 
 
-(define (substr s n)
-  (if (<= (string-length s) n) ""
-      (substring s n)))
-
-
-; String Number -> [List-of String]
-; partition a string into several substrings of a given size
-(check-expect (partition "abcdefgh" 3) (list "abc" "def" "gh"))
-(check-expect (partition "abcd" 2) (list "ab" "cd"))
-(check-expect (partition "abcd" 1) (list "a" "b" "c" "d"))
-
-(define (partition s n)
-  (local ((define leftover (substr s n)))
-    (cond [(equal? leftover "") (list s)]
-          [else (cons (substring s 0 n) (partition leftover n))])))
 
 
 
-
-
-(filter < lon)
-(filter (lambda (x) (not (< x n))))
